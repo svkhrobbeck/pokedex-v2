785dd6c8d9112398d9260f13bdc595ad043587d4
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-12 11:55:06 +0500

    fix: :rotating_light: remove unnecessary plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-refresh", "simple-import-sort", "import", "unused-imports"],
+  plugins: ["@typescript-eslint", "react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
